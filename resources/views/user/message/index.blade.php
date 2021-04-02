a9f31ccdefb1407a8f5815603daf414a5415e124
Maizied <mdshuvo40@gmail.com>
2021-04-02 09:45:04 +0600

    Added V-Cloak in homepage

--- resources/views/user/message/index.blade.php
+++ resources/views/user/message/index.blade.php
@@ -53,17 +53,17 @@
                                 
                         <div v-for="item in messages" class="list-group-item  align-items-center" :class="{'right': item.type==1}" style="border: 0px solid ;">
                             
-                                <div v-if="item.type == 1" style="margin-left: 50%; margin-bottom:5px;">
-                                    <div class="time" style="font-size:10px; margin-left: 52%;"> <i class="far fa-clock"></i> @{{new Date(item.created_at).toLocaleString()}}</div>
+                                <div v-cloak v-if="item.type == 1" style="margin-left: 50%; margin-bottom:5px;">
+                                    <div v-cloak class="time" style="font-size:10px; margin-left: 52%;"> <i class="far fa-clock"></i> @{{new Date(item.created_at).toLocaleString()}}</div>
                                     {{-- <strong>{{Auth::user()->first_name.' '.Auth::user()->last_name}}</strong> --}}
-                                    <div class="message txtWhitecolor"  style="text-align: left; width: 100%; border-radius: 10px; border: 1px solid #d2d2d2;
+                                    <div v-cloak class="message txtWhitecolor"  style="text-align: left; width: 100%; border-radius: 10px; border: 1px solid #d2d2d2;
                                     padding: 10px; text-align: right; font-size: 18px;">
                                         <pre style="white-space: break-spaces !important;">@{{item.message}}</pre>
                                     </div>
                                 </div>
                                 <div v-else>
-                                    <div class="time" style="font-size:10px;" ><i class="far fa-clock"></i> @{{new Date(item.created_at).toLocaleString()}}</div>
-                                    <div class="name d-flex justify-content-between message" style="text-align: left;width: 60%; border-radius: 10px; border: 1px solid #d2d2d2;
+                                    <div  v-cloak class="time" style="font-size:10px;" ><i class="far fa-clock"></i> @{{new Date(item.created_at).toLocaleString()}}</div>
+                                    <div v-cloak class="name d-flex justify-content-between message" style="text-align: left;width: 60%; border-radius: 10px; border: 1px solid #d2d2d2;
                                     padding: 10px; text-align: left; font-size: 18px;">
                                         <pre style="white-space: break-spaces !important;">@{{item.message}}</pre>
                                     </div>
