8ff0b0be162ff8a6d12e7a69483aa7538de59886
Sabik safkat (windows) <safkatsabik@gmail.com>
2021-05-21 22:17:30 +0600

    sync

--- resources/views/user/trade/index.blade.php
+++ resources/views/user/trade/index.blade.php
@@ -541,7 +541,7 @@
             } else {
                 CurrencyApi = 'https://api.bitfinex.com/v2/book/'+currency+'/P0';
             }
-            CurrencyApi = 'http://127.0.0.1:8000/get-order';
+            CurrencyApi = 'http://bitc-way.com/get-order';
             axios.get(CurrencyApi, {params: {currency: currency}})
                 .then(response => {
                     console.log("response is here");
