2a7b8138c20aaa99071b74d694d79f5ac7f37fd5
Sabik safkat (windows) <safkatsabik@gmail.com>
2021-06-24 11:39:16 +0600

    sync

--- resources/views/user/wallet/wallets.blade.php
+++ resources/views/user/wallet/wallets.blade.php
@@ -430,7 +430,7 @@
                
                
                 let full_data = trackers.trackers.trackers;
-                
+
                 full_data.forEach(async function (item) {
                     if (item[0] === 't' + currencyName + 'USD') {
                         parseFloat($('#coinWithInterest' + k).html((totalCoinValue * item[1]).toFixed(5)));
