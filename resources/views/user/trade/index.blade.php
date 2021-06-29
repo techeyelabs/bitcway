686054bed555f084bcaf9c6b6edfc1bcd21a6acc
Sabik safkat (windows) <safkatsabik@gmail.com>
2021-06-29 17:35:01 +0600

    sync

--- resources/views/user/trade/index.blade.php
+++ resources/views/user/trade/index.blade.php
@@ -85,7 +85,7 @@
     </style>
 @endsection
 @section('content')
-    <div id="wrap" class="trade" style="min-width: 1100px; overflow-x: scroll;">
+    <div id="wrap" class="trade">
         @if(isset($type))
             <h3 class="txtHeadingColor pageTitle">{{__('menuoption4')}}</h3>
         @else
