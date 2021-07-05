e9c169cdae0026a3dcf7db5e766b7454a919fb36
Sabik safkat (windows) <safkatsabik@gmail.com>
2021-07-05 16:18:41 +0600

    limit cron job testing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->call('App\Http\Controllers\CronController@MyAction')->everyMinute();
+         $schedule->call('App\Http\Controllers\LimitCronController@limitCronJob')->everyMinute();
         // $schedule->call(function () {
         //     // DB::table('users')->whereYear('last_login_at','<',date('Y')-1)->delete();
         //     echo 'hi';
